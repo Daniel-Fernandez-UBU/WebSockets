91d9658525f955694326d634de7fa104fddcf239
Daniel <dfb1001@alu.ubu.es>
2024-05-22 20:13:32 +0200

    Generamos la clase Message y sus getters y setters

---
+++ WebSocketApp/src/main/java/es/ubu/lsi/WebSocketApp/Model/Message.java
@@ -0,0 +1,23 @@
+package es.ubu.lsi.WebSocketApp.Model;
+
+public class Message {
+	
+	private String form;
+	private String text;
+	
+	public String getForm() {
+		return form;
+	}
+	public void setForm(String form) {
+		this.form = form;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+
+}
