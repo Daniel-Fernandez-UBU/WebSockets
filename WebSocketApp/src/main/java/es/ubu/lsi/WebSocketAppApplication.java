751b8332e40d5bb9c3ffb5e9b6c53e1dcf8cf761
Daniel <dfb1001@alu.ubu.es>
2024-05-22 20:35:52 +0200

    Añadimos el proyecto base con Sprint Boot Initialzr

---
+++ WebSocketApp/src/main/java/es/ubu/lsi/WebSocketAppApplication.java
@@ -0,0 +1,13 @@
+package es.ubu.lsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketAppApplication.class, args);
+	}
+
+}
