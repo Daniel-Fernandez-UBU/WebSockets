04872df239b4650326adcbcdc5ee3314f2bd9c9b
Daniel <dfb1001@alu.ubu.es>
2024-05-24 10:58:49 +0200

    Añadidos comentarios a la clase ChatController

--- WebSocketApp/src/main/java/es/ubu/lsi/controller/ChatController.java
+++ WebSocketApp/src/main/java/es/ubu/lsi/controller/ChatController.java
@@ -13,13 +13,25 @@ import org.springframework.stereotype.Controller;
 import es.ubu.lsi.model.ChatMessage;
 import es.ubu.lsi.service.UserService;
 
-
+/**
+ * Class ChatController.
+ * 
+ * Clase controladora para el chat, que añade nuevos usuarios al chat o envía los mensajes al resto de destinatarios
+ * 
+ */
 @Controller
 public class ChatController {
 	
+	/** inyección del servicio de usuario */
 	@Autowired
 	private UserService userService;
 
+	/**
+	 * Método ChatMessage.
+	 * 
+	 * @param chatMessage - recibe un objeto de tipo chatMessage
+	 * @return chatessage formateado con los datos necesarios
+	 */
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
@@ -27,10 +39,18 @@ public class ChatController {
     	String currentDate = new SimpleDateFormat().format(date);
     	chatMessage.setFrom_id(currentDate);
     	chatMessage.setFrom_level(userService.userLevel(chatMessage.getFrom()));
-    	System.out.println("User level: " + userService.userLevel(chatMessage.getFrom()));
         return chatMessage;
     }
 
+    /**
+     * Méodo addUser.
+     * 
+     * Añade nuevos usuarios al chat.
+     * 
+     * @param chatMessage - parámetro que contiene el nombre de usuario.
+     * @param headerAccessor - cabecera d
+     * @return chatMessage
+     */
     @MessageMapping("/chat.addUser")
     @SendTo("/topic/public")
     public ChatMessage addUser(@Payload ChatMessage chatMessage, 
